585c6e97d95a0525858c6788343ee0592f53c3cd
wu-tzu-hsuan <>
2018-08-14 23:50:12 +0800

    banner

---
+++ js/banner.js
@@ -0,0 +1,8 @@
+$(function () {
+	$('button').click(function() {
+		var l = 5;
+		var $banner = $(this).closest('.container').find('.banner');
+		var r = parseInt($banner.attr('data-i')) + parseInt($(this).data('active'));		
+		$banner.attr('data-i', r <= 0 ? l : (l >= r ? r : 1) );
+	});
+});
