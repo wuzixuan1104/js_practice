e0a87c64092950ff6a8b03003dc59b84c20054af
吳姿萱 <cherry51120@gmail.com>
2018-08-16 10:59:06 +0800

    自動輪播完成！

--- js/public.js
+++ js/public.js
@@ -6,5 +6,30 @@
  */
  
 $(function () {
+  var $a = $('.banner').children();
+  $a.last().clone().prependTo('.banner') && $a.first().clone().appendTo('.banner');
+  var lIndex = $('.banner').children().length-1;
+  $('.box').css('width', (lIndex+1)*50 + 'px' );
+  var now = 1;
 
+  $('button').click(function() {
+    switch($(this).data('active')) {
+      case 1: //right
+        if(now == lIndex || ++now == lIndex) {
+          $( '.box' ).animate({ "left": "-=100%" }, { duration: "fast", complete: function() { $('.box').css("left", 0);}});
+          now = 1;
+        } else {
+          $('.box').animate({"left": "-=100%"},"fast");
+        }
+        break;
+      case -1: //left
+        if(now == 1 || --now == 1) {
+          $( '.box' ).animate({ "left": "+=100%" }, { duration: "fast", complete: function() { $('.box').css("left", "-500%");}});
+          now = lIndex;
+        } else {
+          $('.box').animate({"left": "+=100%"},"fast");
+        }
+        break;
+    }
+  });
 });
